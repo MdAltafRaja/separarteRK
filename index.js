d6b9a9fc540a1ce1fad822db56612a61984a5082
aashutosh03Juspay <aashutosh.bansal@juspay.in>
2022-09-07 16:21:58 +0530

    update

---
+++ index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {AppRegistry, StyleSheet, Text, View} from 'react-native';
+import {name as appName} from './app.json';
+
+const HelloWorld = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.hello}>Hello, World</Text>
+    </View>
+  );
+};
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  hello: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
+
+AppRegistry.registerComponent(appName, () => HelloWorld);
